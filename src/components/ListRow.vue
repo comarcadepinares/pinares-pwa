3d7c8441db51bb48d2955944da66d67ff933e46e
Simon Martín <oleurud@gmail.com>
2019-08-11 22:05:57 +0200

    list row

---
+++ src/components/ListRow.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list-row">
+    <div class="img">
+      <img style="width: 100%; display: block;" :src="image" :alt="title">
+    </div>
+    <div class="text">
+      <h6>{{title}}</h6>
+      <p class="card-text">{{description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'listRow',
+  props: {
+    image: String,
+    title: String,
+    description: String
+  }
+}
+</script>
--- src/views/FullMap.vue
+++ src/views/FullMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="fullMap">
-    <Map :category="category"></Map>
+    <Map :category="category" @toggleMenu="toggleMenu"></Map>
 
     <button id="menuButton" type="button" class="btn btn-primary" @click="toggleMenu">Menu</button>
     <div id="menu">
@@ -19,33 +19,11 @@
         </div>
       </div>
       <div class="menuSection">
-        <div class="list-row">
-          <div class="img">
-            <img style="width: 100%; display: block;" src="/img/Duruelo.png" alt="Card image">
-          </div>
-          <div class="text">
-            <h6>La lalala</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div class="list-row">
-          <div class="img">
-            <img style="width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
-          </div>
-          <div class="text">
-            <h6>La lalala</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div class="list-row">
-          <div class="img">
-            <img style="width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
-          </div>
-          <div class="text">
-            <h6>La lalala</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
+        <ListRow v-for="entity in entities" :key="entity.id"
+          :image="entity.image"
+          :title="entity.name"
+          :description="entity.description"
+        ></ListRow>
       </div>
     </div>
   </div>
@@ -54,6 +32,7 @@
 <script>
 import ws from '@/services/webservice'
 import Map from '@/components/Map'
+import ListRow from '@/components/ListRow'
 
 export default {
   name: 'fullMap',
@@ -69,6 +48,11 @@ export default {
     }
 
     this.getEntities()
+  },
+  computed: {
+    entities () {
+      return this.$store.state[this.category]
+    }
   },
   methods: {
     getEntities () {
@@ -133,7 +117,8 @@ export default {
     }
   },
   components: {
-    Map
+    Map,
+    ListRow
   }
 }
 </script>
